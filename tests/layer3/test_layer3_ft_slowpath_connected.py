80a4bdd6206e97f7002a6a93b59fd57d0b53bc10
Vasanth Viswanathan <vasanth.viswanathan@hpe.com>
2015-09-24 15:27:47 -0700

    Cleanup script to opsvsi and pep8
    This commit includes changes to move from halon
    to opsvsi. Also changes to conform to pep8 guidelines.
    
    Change-Id: I93ae3770ec2f71448582a76c8b85ae677c1b0c8f
    Signed-off-by: Vasanth Viswanathan <vasanth.viswanathan@hpe.com>

--- tests/layer3/test_layer3_ft_slowpath_connected.py
+++ tests/layer3/test_layer3_ft_slowpath_connected.py
@@ -15,134 +15,134 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import pytest
-from halonvsi.docker import *
-from halonvsi.halon import *
-from halonutils.halonutil import *
+from opsvsi.docker import *
+from opsvsi.opsvsitest import *
+from opsvsiutils.systemutil import *
 
-class slowRoutingTests( HalonTest ):
 
-  def setupNet(self):
-    # if you override this function, make sure to
-    # either pass getNodeOpts() into hopts/sopts of the topology that
-    # you build or into addHost/addSwitch calls
-    self.net = Mininet(topo=SingleSwitchTopo(k=2,
-                                             hopts=self.getHostOpts(),
-                                             sopts=self.getSwitchOpts()),
-                                             switch=HalonSwitch,
-                                             host=HalonHost,
-                                             link=HalonLink, controller=None,
-                                             build=True)
+class slowRoutingTests(OpsVsiTest):
 
-  def slow_routing_direct_connected(self):
-    info("########## Verify slow path routing for directly connected hosts ##########\n")
-    # configuring Halon, in the future it would be through
-    # proper Halon commands
-    s1 = self.net.switches[ 0 ]
-    h1 = self.net.hosts[ 0 ]
-    h2 = self.net.hosts[ 1 ]
+    def setupNet(self):
 
-    # Configure switch s1
-    s1.cmdCLI("configure terminal")
+        host_opts = self.getHostOpts()
+        switch_opts = self.getSwitchOpts()
+        slowpath_topo = \
+            SingleSwitchTopo(k=2, hopts=host_opts, sopts=switch_opts)
+        self.net = Mininet(slowpath_topo, switch=VsiOpenSwitch,
+                           host=Host, link=OpsVsiLink,
+                           controller=None, build=True)
 
-    # Configure interface 1 on switch s1
-    s1.cmdCLI("interface 1")
-    #s1.cmdCLI("no shutdown")
-    s1.cmdCLI("ip address 192.168.1.1/24")
-    time.sleep(2)
-    s1.cmdCLI("ipv6 address 2000::1/120")
-    s1.cmdCLI("exit")
+    def slow_routing_direct_connected(self):
+        info("########## Verify slow path routing for "
+             "directly connected hosts ##########\n")
+        s1 = self.net.switches[0]
+        h1 = self.net.hosts[0]
+        h2 = self.net.hosts[1]
 
-    # Configure interface 2 on switch s1
-    s1.cmdCLI("interface 2")
-    #s1.cmdCLI("no shutdown")
-    s1.cmdCLI("ip address 192.168.2.1/24")
-    time.sleep(2)
-    s1.cmdCLI("ipv6 address 2002::1/120")
-    s1.cmdCLI("exit")
+        # Configure switch s1
+        s1.cmdCLI("configure terminal")
 
-    # HALON_TODO: Using "no shutdown" needs sleep of 10 seconds.
-    # Uncomment below line and remove ovs-vsctl command once issue is resolved
+        # Configure interface 1 on switch s1
+        s1.cmdCLI("interface 1")
+        # s1.cmdCLI("no shutdown")
+        s1.cmdCLI("ip address 192.168.1.1/24")
+        time.sleep(2)
+        s1.cmdCLI("ipv6 address 2000::1/120")
+        s1.cmdCLI("exit")
 
-    #time.sleep(10)
+        # Configure interface 2 on switch s1
+        s1.cmdCLI("interface 2")
+        # s1.cmdCLI("no shutdown")
+        s1.cmdCLI("ip address 192.168.2.1/24")
+        time.sleep(2)
+        s1.cmdCLI("ipv6 address 2002::1/120")
+        s1.cmdCLI("exit")
 
-    # Bring interface 1 up
-    s1.cmd("/usr/bin/ovs-vsctl set interface 1 user_config:admin=up")
+        # TODO: Using "no shutdown" needs sleep of 10 seconds.
+        # Uncomment below line and remove ovs-vsctl command
+        # once issue is resolved
 
-    # Bring interface 2 up
-    s1.cmd("/usr/bin/ovs-vsctl set interface 2 user_config:admin=up")
+        # time.sleep(10)
 
-    # Configure host 1
-    info("Configuring host 1 with 192.168.1.2/24\n")
-    h1.cmd("ip addr add 192.168.1.2/24 dev h1-eth0")
-    h1.cmd("ip route add 192.168.2.0/24 via 192.168.1.1");
-    info("Configuring host 1 with 2000::2/120\n")
-    h1.cmd("ip addr add 2000::2/120 dev h1-eth0")
-    h1.cmd("ip route add 2002::0/120 via 2000::1")
+        # Bring interface 1 up
+        s1.ovscmd("/usr/bin/ovs-vsctl set interface 1 user_config:admin=up")
 
-    # Configure host 2
-    info("Configuring host 2 with 192.168.2.2/24\n")
-    h2.cmd("ip addr add 192.168.2.2/24 dev h2-eth0")
-    h2.cmd("ip route add 192.168.1.0/24 via 192.168.2.1");
-    info("Configuring host 2 with 2002::2/120\n")
-    h2.cmd("ip addr add 2002::2/120 dev h2-eth0")
-    h2.cmd("ip route add 2000::0/120 via 2002::1")
+        # Bring interface 2 up
+        s1.ovscmd("/usr/bin/ovs-vsctl set interface 2 user_config:admin=up")
 
-    # Ping from host 1 to switch
-    info("Ping s1 from h1\n")
-    output = h1.cmd("ping 192.168.1.1 -c2")
-    status = parsePing(output)
-    assert status, "Ping Failed\n"
-    info("Ping Success\n")
+        # Configure host 1
+        info("Configuring host 1 with 192.168.1.2/24\n")
+        h1.cmd("ip addr add 192.168.1.2/24 dev h1-eth0")
+        h1.cmd("ip route add 192.168.2.0/24 via 192.168.1.1")
+        info("Configuring host 1 with 2000::2/120\n")
+        h1.cmd("ip addr add 2000::2/120 dev h1-eth0")
+        h1.cmd("ip route add 2002::0/120 via 2000::1")
 
-    # Ping from host 2 to switch
-    info("Ping s1 from h2\n")
-    output = h2.cmd("ping 192.168.2.1 -c2")
-    status = parsePing(output)
-    assert status, "Ping Failed"
-    info("Ping Success\n")
+        # Configure host 2
+        info("Configuring host 2 with 192.168.2.2/24\n")
+        h2.cmd("ip addr add 192.168.2.2/24 dev h2-eth0")
+        h2.cmd("ip route add 192.168.1.0/24 via 192.168.2.1")
+        info("Configuring host 2 with 2002::2/120\n")
+        h2.cmd("ip addr add 2002::2/120 dev h2-eth0")
+        h2.cmd("ip route add 2000::0/120 via 2002::1")
 
-    # Ping from host 1 to host 2
-    info("Ping h2 from h1\n")
-    output = h1.cmd("ping 192.168.2.2 -c2")
-    status = parsePing(output)
-    assert status, "Ping Failed"
-    info("Ping Success\n")
+        # Ping from host 1 to switch
+        info("Ping s1 from h1\n")
+        output = h1.cmd("ping 192.168.1.1 -c2")
+        status = parsePing(output)
+        assert status, "Ping Failed\n"
+        info("Ping Success\n")
 
-    # Ping from host 1 to switch
-    info("IPv6 Ping s1 from h1\n")
-    output = h1.cmd("ping6 2000::1 -c2")
-    status = parsePing(output)
-    assert status, "Ping Failed"
-    info("Ping Success\n")
+        # Ping from host 2 to switch
+        info("Ping s1 from h2\n")
+        output = h2.cmd("ping 192.168.2.1 -c2")
+        status = parsePing(output)
+        assert status, "Ping Failed"
+        info("Ping Success\n")
 
-    # Ping from host 2 to switch
-    info("IPv6 Ping s1 from h2\n")
-    output = h2.cmd("ping6 2002::1 -c2")
-    status = parsePing(output)
-    assert status, "Ping Failed"
-    info("Ping Success\n")
+        # Ping from host 1 to host 2
+        info("Ping h2 from h1\n")
+        output = h1.cmd("ping 192.168.2.2 -c2")
+        status = parsePing(output)
+        assert status, "Ping Failed"
+        info("Ping Success\n")
 
-    # Ping from host 1 to host 2
-    info("IPv6 Ping h2 from h1\n")
-    output = h1.cmd("ping6 2002::2 -c2")
-    status = parsePing(output)
-    assert status, "Ping Failed"
-    info("Ping Success\n")
+        # Ping from host 1 to switch
+        info("IPv6 Ping s1 from h1\n")
+        output = h1.cmd("ping6 2000::1 -c2")
+        status = parsePing(output)
+        assert status, "Ping Failed"
+        info("Ping Success\n")
 
-    info("########## Slow path routing for directly connected hosts passed ##########\n")
+        # Ping from host 2 to switch
+        info("IPv6 Ping s1 from h2\n")
+        output = h2.cmd("ping6 2002::1 -c2")
+        status = parsePing(output)
+        assert status, "Ping Failed"
+        info("Ping Success\n")
+
+        # Ping from host 1 to host 2
+        info("IPv6 Ping h2 from h1\n")
+        output = h1.cmd("ping6 2002::2 -c2")
+        status = parsePing(output)
+        assert status, "Ping Failed"
+        info("Ping Success\n")
+
+        info("########## Slow path routing for directly "
+             "connected hosts passed ##########\n")
+
 
 class Test_slow_routing:
 
-  def setup_class(cls):
-    Test_slow_routing.test = slowRoutingTests()
+    def setup_class(cls):
+        Test_slow_routing.test = slowRoutingTests()
 
-  # Test for slow routing between directly connected hosts
-  def test_slow_routing_direct_connected(self):
-    self.test.slow_routing_direct_connected()
+    # Test for slow routing between directly connected hosts
+    def test_slow_routing_direct_connected(self):
+        self.test.slow_routing_direct_connected()
 
-  def teardown_class(cls):
-    Test_slow_routing.test.net.stop()
+    def teardown_class(cls):
+        Test_slow_routing.test.net.stop()
 
-  def __del__(self):
-    del self.test
+    def __del__(self):
+        del self.test
--- tests/layer3/test_layer3_ft_static_routes.py
+++ tests/layer3/test_layer3_ft_static_routes.py
@@ -15,11 +15,12 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from halonvsi.docker import *
-from halonvsi.halon import *
-from halonutils.halonutil import *
+from opsvsi.docker import *
+from opsvsi.opsvsitest import *
+from opsvsiutils.systemutil import *
 
-class myTopo( Topo ):
+
+class myTopo(Topo):
     """Custom Topology Example
     H1[h1-eth0]<--->[1]S1[2]<--->[2]S2[1]<--->[h2-eth0]H2
     """
@@ -28,36 +29,38 @@ class myTopo( Topo ):
         self.hsts = hsts
         self.sws = sws
 
-        #Add list of hosts
-        for h in irange( 1, hsts):
-            host = self.addHost( 'h%s' %h)
+        # Add list of hosts
+        for h in irange(1, hsts):
+            host = self.addHost('h%s' % h)
 
-        #Add list of switches
+        # Add list of switches
         for s in irange(1, sws):
-            switch = self.addSwitch( 's%s' %s)
+            switch = self.addSwitch('s%s' % s)
 
-        #Add links between nodes based on custom topo
+        # Add links between nodes based on custom topo
         self.addLink('h1', 's1')
         self.addLink('h2', 's2')
         self.addLink('s1', 's2')
 
-class staticRouteTest( HalonTest ):
+
+class staticRouteTest(OpsVsiTest):
 
     def setupNet(self):
-        self.net = Mininet(topo=myTopo(hsts=2, sws=2,
-                                       hopts=self.getHostOpts(),
-                                       sopts=self.getSwitchOpts()),
-                                       switch=HalonSwitch,
-                                       host=HalonHost,
-                                       link=HalonLink, controller=None,
-                                       build=True)
+
+        host_opts = self.getHostOpts()
+        switch_opts = self.getSwitchOpts()
+        static_routes_topo = \
+            myTopo(hsts=2, sws=2, hopts=host_opts, sopts=switch_opts)
+        self.net = Mininet(static_routes_topo, switch=VsiOpenSwitch,
+                           host=Host, link=OpsVsiLink,
+                           controller=None, build=True)
 
     def testConfigure(self):
         info('\n########## Configuring the topology ##########\n')
-        s1 = self.net.switches[ 0 ]
-        s2 = self.net.switches[ 1 ]
-        h1 = self.net.hosts[ 0 ]
-        h2 = self.net.hosts[ 1 ]
+        s1 = self.net.switches[0]
+        s2 = self.net.switches[1]
+        h1 = self.net.hosts[0]
+        h2 = self.net.hosts[1]
 
         # Configure switch s1
         s1.cmdCLI("configure terminal")
@@ -73,13 +76,11 @@ class staticRouteTest( HalonTest ):
         s1.cmdCLI("ip address 10.0.20.1/24")
         s1.cmdCLI("ipv6 address 2001::1/120")
         s1.cmdCLI("exit")
-        #s1.cmd("/usr/bin/ovs-vsctl add-vrf-port vrf_default 2")
 
-        # HALON_TODO: vtysh infra has an issue while adding and attaching
+        # TODO: vtysh infra has an issue while adding and attaching
         # multiple interfaces to the vrf. Once that part of the code is fixed,
         # the following line can be removed. As of now, sometimes the interface
         # does not get added to the vrf.
-        #s1.cmd("/usr/bin/ovs-vsctl set port 2 ip4_address=10.0.20.1/24 ip6_address='2001\:\:1/120'")
 
         info('sw1 configured\n')
 
@@ -97,13 +98,11 @@ class staticRouteTest( HalonTest ):
         s2.cmdCLI("ip address 10.0.20.2/24")
         s2.cmdCLI("ipv6 address 2001::2/120")
         s2.cmdCLI("exit")
-        #s2.cmd("/usr/bin/ovs-vsctl add-vrf-port vrf_default 2")
 
-        # HALON_TODO: vtysh infra has an issue while adding and attaching
+        # TODO: vtysh infra has an issue while adding and attaching
         # multiple interfaces to the vrf. Once that part of the code is fixed,
         # the following line can be removed. As of now, sometimes the interface
         # does not get added to the vrf.
-        #s2.cmd("/usr/bin/ovs-vsctl set port 2 ip4_address=10.0.20.2/24 ip6_address='2001\:\:2/120'")
         info('sw2 configured\n')
 
         # Configure host 1
@@ -120,7 +119,7 @@ class staticRouteTest( HalonTest ):
 
         info('host2 configured\n')
 
-        #Add IPv4 static route on s1 and s2
+        # Add IPv4 static route on s1 and s2
         s1.cmdCLI("ip route 10.0.30.0/24 10.0.20.2")
         info('static route on sw1 configured\n')
 
@@ -143,172 +142,100 @@ class staticRouteTest( HalonTest ):
         h2.cmd("ip route add 2000::0/120 via 2002::2")
         h2.cmd("ip route add 2001::0/120 via 2002::2")
 
-        s1.cmd("/usr/bin/ovs-vsctl set interface 1 user_config:admin=up")
-        s1.cmd("/usr/bin/ovs-vsctl set interface 2 user_config:admin=up")
+        s1.ovscmd("/usr/bin/ovs-vsctl set interface 1 user_config:admin=up")
+        s1.ovscmd("/usr/bin/ovs-vsctl set interface 2 user_config:admin=up")
 
-        s2.cmd("/usr/bin/ovs-vsctl set interface 1 user_config:admin=up")
-        s2.cmd("/usr/bin/ovs-vsctl set interface 2 user_config:admin=up")
+        s2.ovscmd("/usr/bin/ovs-vsctl set interface 1 user_config:admin=up")
+        s2.ovscmd("/usr/bin/ovs-vsctl set interface 2 user_config:admin=up")
 
         info('admin up configured on sw1 & sw2\n')
         info('\n########## Configuration complete ##########\n')
 
-
     def testV4(self):
         info('\n########## IPv4 Ping test ##########\n')
-        h1 = self.net.hosts[ 0 ]
-        h2 = self.net.hosts[ 1 ]
+        h1 = self.net.hosts[0]
+        h2 = self.net.hosts[1]
         # Ping host2 from host1
-        info( '\n### Ping host2 from host1 ###\n')
+        info('\n### Ping host2 from host1 ###\n')
         ret = h1.cmd("ping -c 1 10.0.30.1")
 
         status = parsePing(ret)
 
-        #return code means whether the test is successful
+        # Return code means whether the test is successful
         if status:
             info('Ping Passed!\n\n')
         else:
             info('Ping Failed!\n\n')
-            #return False
+            # return False
 
         # Ping host1 from host2
-        info( '\n### Ping host1 from host2 ###\n')
+        info('\n### Ping host1 from host2 ###\n')
         ret = h2.cmd("ping -c 1 10.0.10.1")
 
         status = parsePing(ret)
 
-        #return code means whether the test is successful
+        # Return code means whether the test is successful
         if status:
             info('Ping Passed!\n\n')
         else:
             info('Ping Failed!\n\n')
-            #return False
+            # return False
 
         info('\n########## IPv4 Ping completed ##########\n')
 
     def testV6(self):
         info('\n########## IPv6 Ping test ##########\n')
-        h1 = self.net.hosts[ 0 ]
-        h2 = self.net.hosts[ 1 ]
-        s1 = self.net.switches[ 0 ]
-        s2 = self.net.switches[ 1 ]
+        h1 = self.net.hosts[0]
+        h2 = self.net.hosts[1]
+        s1 = self.net.switches[0]
+        s2 = self.net.switches[1]
 
-        # HALON_TODO: For IPv6, we see that even after setting static routes
-        # correctly, direct ping from h1 to h2 is not taking place. After subsequent
-        # pings between adjacent devices, end to end ping works. We suspect that there
-        # is some learning going on in the kernel as regards to neighbour advertisements.
+        # TODO: For IPv6, we see that even after setting static routes
+        # correctly, direct ping from h1 to h2 is not taking place.
+        # After subsequent pings between adjacent devices,
+        # end to end ping works. We suspect that there is some learning
+        # going on in the kernel as regards to neighbour advertisements.
         # Ping s1 from host1
-        info( '### Ping s1 from host1 ###\n')
+        info('### Ping s1 from host1 ###\n')
         ret = h1.cmd("ping6 -c 1 2000::2")
 
         status = parsePing(ret)
 
-        #return code means whether the test is successful
+        # Return code means whether the test is successful
         if status:
             info('Ping Passed!\n\n')
         else:
             info('Ping Failed!\n\n')
-            #return False
+            # return False
 
         # Ping s2 from h2
-        info( '### Ping s2 from h2 ###\n')
+        info('### Ping s2 from h2 ###\n')
         ret = h2.cmd("ping6 -c 1 2002::2")
 
         status = parsePing(ret)
 
-        #return code means whether the test is successful
+        # Return code means whether the test is successful
         if status:
             info('Ping Passed!\n\n')
         else:
             info('Ping Failed!\n\n')
-            #return False
+            # return False
 
         # Ping s2 from s1
-        info( '### Ping s2 from s1 ###\n')
+        info('### Ping s2 from s1 ###\n')
         ret = s1.cmd("ip netns exec swns ping6 -c 1 2001::2")
 
         status = parsePing(ret)
 
-        #return code means whether the test is successful
+        # Return code means whether the test is successful
         if status:
             info('Ping Passed!\n\n')
         else:
             info('Ping Failed!\n\n')
-            #return False
+            # return False
 
         # Ping host2 from host1
-        info( '### Ping host2 from host1 ###\n')
-        ret = h1.cmd("ping6 -c 1 2002::1")
-
-        status = parsePing(ret)
-
-        #return code means whether the test is successful
-        if status:
-            info('Ping Passed!\n\n')
-        else:
-            info('Ping Failed!\n\n')
-            #return False
-
-        # Ping host1 from host2
-        info( '\n### Ping host1 from host2 ###\n')
-        ret = h2.cmd("ping6 -c 1 2000::1")
-
-        status = parsePing(ret)
-
-        #return code means whether the test is successful
-        if status:
-            info('Ping Passed!\n\n')
-        else:
-            info('Ping Failed!\n\n')
-            #return False
-
-        info('\n########## IPv6 Ping completed ##########\n')
-
-    def testV4_route_delete(self):
-        h1 = self.net.hosts[ 0 ]
-        h2 = self.net.hosts[ 1 ]
-        s1 = self.net.switches[ 0 ]
-
-        info( '\n######### Verify deletion of IPv4 static routes ##########\n')
-        # Ping host2 from host1
-        info( '\n### Ping host1 from host2 ###\n')
-        ret = h1.cmd("ping -c 1 10.0.30.1")
-
-        status = parsePing(ret)
-
-        #return code means whether the test is successful
-        if status:
-            info('Ping Passed!\n\n')
-        else:
-            info('Ping Failed!\n\n')
-            #return False
-
-        # Delete IPv4 route on switch1 towards host2 network
-        info( '\n### Delete ip route on sw1 to h2 network ###\n')
-        s1.cmdCLI("no ip route 10.0.30.0/24 10.0.20.2")
-
-        # Ping host1 from host2
-        info( '\n### Ping host1 from host2, it should fail ###\n')
-        ret = h1.cmd("ping -c 1 10.0.30.1")
-
-        status = parsePing(ret)
-        # Test successful if ping fails
-        if not status:
-            info('Success: Ping Failed!\n\n')
-        else:
-            info('Failed: Ping Successful!\n\n')
-            #return False
-
-        info('\n########## IPv4 route delete test completed ##########\n')
-
-    def testV6_route_delete(self):
-        h1 = self.net.hosts[ 0 ]
-        h2 = self.net.hosts[ 1 ]
-        s1 = self.net.switches[ 0 ]
-
-        info( '\n######### Verify deletion of IPv6 static routes ##########\n')
-
-        # Ping host1 from host2
-        info( '### Ping host1 from host2 ###\n')
+        info('### Ping host2 from host1 ###\n')
         ret = h1.cmd("ping6 -c 1 2002::1")
 
         status = parsePing(ret)
@@ -318,14 +245,86 @@ class staticRouteTest( HalonTest ):
             info('Ping Passed!\n\n')
         else:
             info('Ping Failed!\n\n')
-            #return False
+            # return False
+
+        # Ping host1 from host2
+        info('\n### Ping host1 from host2 ###\n')
+        ret = h2.cmd("ping6 -c 1 2000::1")
+
+        status = parsePing(ret)
+
+        # Return code means whether the test is successful
+        if status:
+            info('Ping Passed!\n\n')
+        else:
+            info('Ping Failed!\n\n')
+            # return False
+
+        info('\n########## IPv6 Ping completed ##########\n')
+
+    def testV4_route_delete(self):
+        h1 = self.net.hosts[0]
+        h2 = self.net.hosts[1]
+        s1 = self.net.switches[0]
+
+        info('\n######### Verify deletion of IPv4 static routes ##########\n')
+        # Ping host2 from host1
+        info('\n### Ping host1 from host2 ###\n')
+        ret = h1.cmd("ping -c 1 10.0.30.1")
+
+        status = parsePing(ret)
+
+        # Return code means whether the test is successful
+        if status:
+            info('Ping Passed!\n\n')
+        else:
+            info('Ping Failed!\n\n')
+            # return False
+
+        # Delete IPv4 route on switch1 towards host2 network
+        info('\n### Delete ip route on sw1 to h2 network ###\n')
+        s1.cmdCLI("no ip route 10.0.30.0/24 10.0.20.2")
+
+        # Ping host1 from host2
+        info('\n### Ping host1 from host2, it should fail ###\n')
+        ret = h1.cmd("ping -c 1 10.0.30.1")
+
+        status = parsePing(ret)
+        # Test successful if ping fails
+        if not status:
+            info('Success: Ping Failed!\n\n')
+        else:
+            info('Failed: Ping Successful!\n\n')
+            # return False
+
+        info('\n########## IPv4 route delete test completed ##########\n')
+
+    def testV6_route_delete(self):
+        h1 = self.net.hosts[0]
+        h2 = self.net.hosts[1]
+        s1 = self.net.switches[0]
+
+        info('\n######### Verify deletion of IPv6 static routes ##########\n')
+
+        # Ping host1 from host2
+        info('### Ping host1 from host2 ###\n')
+        ret = h1.cmd("ping6 -c 1 2002::1")
+
+        status = parsePing(ret)
+
+        # Return code means whether the test is successful
+        if status:
+            info('Ping Passed!\n\n')
+        else:
+            info('Ping Failed!\n\n')
+            # return False
 
         # Delete IPv6 route to host2 on switch1
-        info( '\n### Delete ipv6 route on sw1 to h2 network ###\n')
+        info('\n### Delete ipv6 route on sw1 to h2 network ###\n')
         s1.cmdCLI("no ipv6 route 2002::0/120 2001::2")
 
         # Ping host1 from host2
-        info( '\n### Ping host1 from host2, it should fail ###\n')
+        info('\n### Ping host1 from host2, it should fail ###\n')
         ret = h1.cmd("ping6 -c 1 2002::1")
 
         status = parsePing(ret)
@@ -335,7 +334,7 @@ class staticRouteTest( HalonTest ):
             info('Success: Ping Failed!\n\n')
         else:
             info('Failed: Ping Successful!\n\n')
-            #return False
+            # return False
 
         info('\n########## IPv6 route delete test completed ##########\n')
 
@@ -353,27 +352,22 @@ class Test_zebra_static_routes_ft:
     def test_testConfigure(self):
         # Function to configure the topology
         self.test.testConfigure()
-        #CLI(self.test.net)
 
     def test_testV4(self):
         # Function to test V4 ping
         self.test.testV4()
-        #CLI(self.test.net)
 
     def test_testV6(self):
         # Function to test V6 ping
         self.test.testV6()
-        #CLI(self.test.net)
 
     def test_testV4_route_delete(self):
         # Function to test V4 route delete
         self.test.testV4_route_delete()
-        #CLI(self.test.net)
 
     def test_testV6_route_delete(self):
         # Function to test V6 route delete
         self.test.testV6_route_delete()
-        #CLI(self.test.net)
 
     def __del__(self):
         del self.test
