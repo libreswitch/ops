7f647e00d805b0f7b465413d6d6af4ead0993e5b
Eduardo Ramirez <eramireza@hpe.com>
2015-10-23 08:19:32 -0700

    Fix for failing REST test for VLAN's PUT
    Change-Id: Id0e80bc30366c6185e8f89cb002ce3d7a2468719
    Signed-off-by: Eduardo Ramirez <eramireza@hpe.com>

--- tests/rest/test_ops-restd_ft_vlans_put.py
+++ tests/rest/test_ops-restd_ft_vlans_put.py
@@ -150,7 +150,11 @@ class configTest (OpsVsiTest):
                         "ports": [],
                         "vlans": [],
                         "datapath_type": "",
-                        "other_config": {},
+                        "other_config": {
+                            "hwaddr": "",
+                            "mac-table-size": "16",
+                            "mac-aging-time": "300"
+                        },
                         "external_ids": {}
                      }
                 }
