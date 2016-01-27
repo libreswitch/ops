OpenSwitchNOS/openswitch-ops

fix: dev: Add sanity check to all ops-vsi REST tests
This sanity check verifies if the system table row and bridge_normal
exist on the switch before starting the tests. It will wait up to
one minute, if after that time the rows are not present, the test
is failed

Change-Id: I59847090830baf2a3e175db5d2697fe732714032
Signed-off-by: Daniela Ramírez <daniela.rch@hpe.com>

tests/rest/utils/utils.py
@@ -19,6 +19,11 @@ import json
 import httplib
 import random
 import urllib
+import time
+
+import pytest
+
+from opsvsi.opsvsitest import *
 
 from copy import deepcopy
 from string import rstrip
@@ -55,7 +60,6 @@ PORT_DATA = {
     },
     "referenced_by": [{"uri": "/rest/v1/system/bridges/bridge_normal"}]
 }
-
 
 def get_switch_ip(switch):
     switch_ip = switch.cmd("python -c \"import socket;\
@@ -307,3 +311,27 @@ def validate_keys_complete_object(json_data):
     assert json_data["status"] is not None, "status key is not present"
 
     return True
+
+
+def rest_sanity_check(switch_ip):
+    info("\nSwitch Sanity Check: Verify if System table row and bridge_normal exist\n")
+    # Check if bridge_normal is ready, loop until ready or timeout finish
+    bridge_path = "/rest/v1/system"
+    system_path = "/rest/v1/system/bridges/bridge_normal"
+    count = 1
+    max_retries = 60 # 1 minute
+    while count <= max_retries:
+        info("\nSwitch Sanity Check: Try count %d \n" % count)
+        status_system, response_system = execute_request(system_path, "GET",
+                                                         None, switch_ip)
+        status_bridge, response_bridge = execute_request(bridge_path, "GET",
+                                                         None, switch_ip)
+        if status_system is httplib.OK and response_system is not None and \
+            status_bridge is httplib.OK and response_bridge is not None:
+            break;
+        count += 1
+        info("\nSwitch Sanity Check: Retrying\n")
+        time.sleep(1)
+
+    assert count <= max, "Switch Sanity check failure: After waiting %d seconds, "\
+        "the switch is still not ready to run the tests" % max_retries

libreswitch/ops

tests/rest/utils/utils.py
@@ -21,7 +21,10 @@ import random
 import urllib
 import ssl
 import os
+import time
+import pytest
 
+from opsvsi.opsvsitest import *
 from copy import deepcopy
 from string import rstrip
 
@@ -291,6 +294,7 @@ def login(dut, user_name, user_password):
     sslcontext.load_verify_locations(src_file)
     conn = httplib.HTTPSConnection(dut.SWITCH_IP, 443, context=sslcontext)
     url = '/login'
+
     body = {'username': user_name, 'password': user_password}
     headers = {"Content-type": "application/x-www-form-urlencoded",
                "Accept": "text/plain"}
@@ -325,3 +329,27 @@ def validate_keys_complete_object(json_data):
     assert json_data["status"] is not None, "status key is not present"
 
     return True
+
+
+def rest_sanity_check(switch_ip):
+    info("\nSwitch Sanity Check: Verify if System table row and bridge_normal exist\n")
+    # Check if bridge_normal is ready, loop until ready or timeout finish
+    system_path = "/rest/v1/system"
+    bridge_path = "/rest/v1/system/bridges/bridge_normal"
+    count = 1
+    max_retries = 60 # 1 minute
+    while count <= max_retries:
+        info("\nSwitch Sanity Check: Try count %d \n" % count)
+        status_system, response_system = execute_request(system_path, "GET",
+                                                         None, switch_ip)
+        status_bridge, response_bridge = execute_request(bridge_path, "GET",
+                                                         None, switch_ip)
+        if status_system is httplib.OK and response_system is not None and \
+            status_bridge is httplib.OK and response_bridge is not None:
+            break;
+        count += 1
+        info("\nSwitch Sanity Check: Retrying\n")
+        time.sleep(1)
+
+    assert count <= max_retries, "Switch Sanity check failure: After waiting %d seconds, "\
+        "the switch is still not ready to run the tests" % max_retries