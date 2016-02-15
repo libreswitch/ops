917da0b78245fc4a49c33c50ae771082cdd41b35
vkez <ezhilan.vk@hpe.com>
2016-02-15 02:07:31 -0800

    Fix: dev : Enabling all mgmt-intf FT testcases.
    Change-Id: I69c29d6b3916faa5b398512e36a30c4f8d465cf3
    Signed-off-by: vkez <ezhilan.vk@hpe.com>

--- tests/mgmt-intf/test_mgmt_intf_ft.py
+++ tests/mgmt-intf/test_mgmt_intf_ft.py
@@ -457,7 +457,6 @@ class mgmtIntfTests(OpsVsiTest):
             s1.cmd("ip netns exec swns ip address flush dev 1")
 
 
-@pytest.mark.skipif(True, reason="Skipping temporarily to debug build failure.")
 class Test_mgmt_intf:
 
     def setup_class(cls):
