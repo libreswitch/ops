0c22aff93895bba4b6f55dd72b78c59b8e2aa285
vijavaru <varun.vijayakumar@hpe.com>
2016-06-10 16:42:37 -0700

    fix : dev: TG - 1111 Severity - High Readding the patch in https://review.openswitch.net/#/c/10214/2 in master which has already been already merged in rel/dill.
    Since the bgp redistribute schema  has to be changed
    ( https://review.openswitch.net/#/c/10067/ ) to fix TG -1111
    ,the redistribute CLI, Quagga support and FT  has to be temporarily removed
    to avoid build failures.
    
    Change-Id: I4f0088347fc237fe22ee9d728b6b492c311a6b44
    Signed-off-by: vijavaru <varun.vijayakumar@hpe.com>

--- tests/bgp/test_bgp_ft_redistribute.py
+++ tests/bgp/test_bgp_ft_redistribute.py
@@ -351,6 +351,8 @@ def configure(**kwargs):
     assert result is True, "BGP neighbor configuration failed for SW3"
     exitContext(switch3)
 
+@pytest.mark.skipif(True, reason="skipped test case temporarly to avoid failure"
+                    " as redistribute schema, cli and quagga support is being changed")
 @pytest.mark.timeout(600)
 class Test_bgp_redistribute_configuration:
     def setup_class(cls):
