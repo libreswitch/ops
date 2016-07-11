72ac56db35725da16152c4efc8a5659b00d03cfb
David Herdoiza Morales <david.herdoiza@hpe.com>
2016-07-11 20:49:24 +0000

    Remove deletion of bridge_normal in test
    TG-1433
    
    Change-Id: I1e2f38f60fb31c2e68379c115d74cba110a0ebb9
    Signed-off-by: David Herdoiza Morales <david.herdoiza@hpe.com>

--- tests/rest/test_rest_ft_interfacesid.py
+++ tests/rest/test_rest_ft_interfacesid.py
@@ -83,32 +83,6 @@ class QueryInterfacesId(OpsVsiTest):
             GET METHOD JSON response validation for Interface name'
         info('### Success in Rest GET system for Interface name ###\n')
 
-        status_code, response_data = execute_request(
-            self.url, "DELETE", json.dumps(DATA), self.SWITCH_IP, False,
-            xtra_header=self.cookie_header)
-
-        assert status_code == httplib.NO_CONTENT, ("Wrong status code %s " %
-                                                   status_code)
-        info('### Success in executing the rest command "DELETE" for url: ' +
-             self.url + ' ###\n')
-
-        info('### Success in executing the rest command \
-        "DELETE for url=/rest/v1/system/interfaces/bridge_normal" ###\n')
-
-        info('### Success in Rest DELETE Interfacesid ###\n')
-
-        status_code, response_data = execute_request(
-            self.url, "GET", None, self.SWITCH_IP, False,
-            xtra_header=self.cookie_header)
-
-        assert status_code == httplib.NOT_FOUND, ("Wrong status code %s " %
-                                                  status_code)
-
-        info('### Success in executing the rest command" "GET" as not \
-             expected for url: ' + self.url + ' ###\n')
-        info('### Success in Rest Checking http code 404 for GET method \
-             #once DELETED Interfacesid ###\n')
-
 
 class Test_interfaces_id:
     def setup(self):
